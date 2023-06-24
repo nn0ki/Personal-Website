a9408c4e045143705861ea369bd0d3c28d7592c1
n0ki <0xn0ki@gmail.com>
2023-06-24 06:28:10 +0000

    a

--- pages/index.jsx
+++ pages/index.jsx
@@ -36,7 +36,7 @@ export default function Home() {
       </div>
 
 
-        {/* <div className="py-20">
+        <div className="py-20">
         <p className="text-3xl text-white font-semibold">GitHub Repositories</p>
 
         <div className="grid grid-cols-1 md:grid-cols-3 w-full gap-4 items-center mt-2">
@@ -115,7 +115,7 @@ export default function Home() {
                 </div>
               ))}
         </div>
-      </div> */}
+      </div>
 
 
     </>
